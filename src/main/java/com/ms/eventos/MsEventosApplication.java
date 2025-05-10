4e58b3f4cf44814ee1a54fc4f3e9ef48b2833445
Ronald <rtineo@sunat.gob.pe>
2025-05-09 21:33:02 -0500

    se sube el proyecto inicial

---
+++ src/main/java/com/ms/eventos/MsEventosApplication.java
@@ -0,0 +1,13 @@
+package com.ms.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/eventos/MsEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
